9a1c864e0be61c8ebf203a0fcd1863bd76001e44
DavonGT <davonaseo@gmail.com>
2025-04-30 23:47:01 +0800

    fixed registration/login errors

--- linux_gamified/settings.py
+++ linux_gamified/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iit*1!%jeb^t5#ci+&6(y-@&g#zhnxc=ib#_v*hmoc&tuo%c99
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
