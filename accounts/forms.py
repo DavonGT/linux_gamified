be6c6bdaa7f4474ab3e50496e45f3451f1cc38e8
kathlyn <villasinkathlyn@gmail.com>
2025-06-02 03:17:25 +0800

    loginz

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,25 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Player
 
-class PlayerRegistrationForm(UserCreationForm):
 
+#kanan mga effects chenelenz la in ha login/register :)))
+class PlayerRegistrationForm(UserCreationForm):
     class Meta:
         model = Player
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': ' '}),
+            'email': forms.EmailInput(attrs={'placeholder': ' '}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['placeholder'] = ' '
+        self.fields['password2'].widget.attrs['placeholder'] = ' '
+
+class PlayerLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = ' '
+        self.fields['password'].widget.attrs['placeholder'] = ' '
